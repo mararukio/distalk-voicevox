69a8f9df54aa6b75e6f2fd8edb5f2e8b50666de8
mararukio <104323737+mararukio@users.noreply.github.com>
2022-05-14 10:58:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,190 +1,83 @@
-import asyncio
 import discord
 from discord.ext import commands
 import os
-import traceback
-import re
-import emoji
-import json
+import random
+import glob
 
-prefix = os.getenv('DISCORD_BOT_PREFIX', default='🦑')
-token = os.environ['DISCORD_BOT_TOKEN']
-voicevox_key = os.environ['VOICEVOX_KEY']
-voicevox_speaker = os.getenv('VOICEVOX_SPEAKER', default='2')
-client = commands.Bot(command_prefix=prefix)
-with open('emoji_ja.json', encoding='utf-8') as file:
-    emoji_dataset = json.load(file)
+TOKEN = "OTY4MTIyODA0MjY2NDkxOTA0.YmaQnw.covHjTTIONxLFEWhWCD-Tp4MQrw"
+
+client = discord.Client()
 
 @client.event
-async def on_ready():
-    presence = f'{prefix}ヘルプ | 0/{len(client.guilds)}サーバー'
-    await client.change_presence(activity=discord.Game(name=presence))
+async def on_message(message):   
 
-@client.event
-async def on_guild_join(guild):
-    presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-    await client.change_presence(activity=discord.Game(name=presence))
+    if message.author.bot:
+        return
 
-@client.event
-async def on_guild_remove(guild):
-    presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-    await client.change_presence(activity=discord.Game(name=presence))
+    if message.content == "$help":
+        await message.channel.send("$pixと入力するとランダムな画像を送信します\n$clipと入力するとランダムな動画を送信します\n$tofuxicと入力するとtofuをいじめることができます\n$kill CENSLOEDと入力することで殺せます。")
 
-@client.command()
-async def 接続(ctx):
-    if ctx.message.guild:
-        if ctx.author.voice is None:
-            await ctx.send('ボイスチャンネルに接続してから呼び出してください。')
-        else:
-            if ctx.guild.voice_client:
-                if ctx.author.voice.channel == ctx.guild.voice_client.channel:
-                    await ctx.send('接続済みです。')
-                else:
-                    await ctx.voice_client.disconnect()
-                    await asyncio.sleep(0.5)
-                    await ctx.author.voice.channel.connect()
-            else:
-                await ctx.author.voice.channel.connect()
+    if message.content == "無名ちゃん":
+        await message.channel.send("https://twitter.com/XzLib?s=20&t=X0-9hXmgcBtz11beTyXBmg")  
 
-@client.command()
-async def 切断(ctx):
-    if ctx.message.guild:
-        if ctx.voice_client is None:
-            await ctx.send('ボイスチャンネルに接続していません。')
-        else:
-            await ctx.voice_client.disconnect()
+    if message.content == "$killcount":
+        global nigga
+        nigga = 0
+        await message.channel.send("killcountを設定しました。") 
+        
+    if message.content == "$kill CENSOLED":
+        await message.channel.send("Censoledを殺しました") 
+        nigga += 1
+        await message.channel.send("今まで殺したCensoledの数は" + str(nigga) + "匹です。") 
+    
+    for i in range(15):
+     if message.content == "$hicensoled":
+         await message.channel.send("<@533547797060780052>")
 
-@client.event
-async def on_message(message):
-    if message.guild.voice_client:
-        if not message.author.bot:
-            if not message.content.startswith(prefix):
-                text = message.content
+    for i in range(15):
+     if message.content == "$tofuxic":
+        await message.channel.send("<@688668113297997846>")
 
-                # Add author's name
-                text = message.author.name + '、' + text
+    for i in range(15):
+     if message.content == "$encha":
+         await message.channel.send("<@759753838734868570>enchant!?!?!?!")  
 
-                # Replace new line
-                text = text.replace('\n', '、')
+    for i in range(15):
+     if message.content == "$風間いろは":
+         await message.channel.send("<@657528099310796820>")   
 
-                # Replace mention to user
-                pattern = r'<@!?(\d+)>'
-                match = re.findall(pattern, text)
-                for user_id in match:
-                    user = await client.fetch_user(user_id)
-                    user_name = f'、{user.name}へのメンション、'
-                    text = re.sub(rf'<@!?{user_id}>', user_name, text)
+    for i in range(15):
+     if message.content == "$なおみ":
+         await message.channel.send("<@715154836445724734>")         
 
-                # Replace mention to role
-                pattern = r'<@&(\d+)>'
-                match = re.findall(pattern, text)
-                for role_id in match:
-                    role = message.guild.get_role(int(role_id))
-                    role_name = f'、{role.name}へのメンション、'
-                    text = re.sub(f'<@&{role_id}>', role_name, text)
+    if message.content == "$yue":
+         await message.channel.send("https://streamable.com/ahmxij")       
 
-                # Replace Unicode emoji
-                text = re.sub(r'[\U0000FE00-\U0000FE0F]', '', text)
-                text = re.sub(r'[\U0001F3FB-\U0001F3FF]', '', text)
-                for char in text:
-                    if char in emoji.UNICODE_EMOJI['en'] and char in emoji_dataset:
-                        text = text.replace(char, emoji_dataset[char]['short_name'])
+    if message.content == "$terada":
+        await message.channel.send(file=discord.File("terada.jpg"))  
 
-                # Replace Discord emoji
-                pattern = r'<:([a-zA-Z0-9_]+):\d+>'
-                match = re.findall(pattern, text)
-                for emoji_name in match:
-                    emoji_read_name = emoji_name.replace('_', ' ')
-                    text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
+    if message.content == "$elf":
+        await message.channel.send(file=discord.File("elf.png"))      
 
-                # Replace URL
-                pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
-                text = re.sub(pattern, '、画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '、URL', text)
+    if message.content == '$pix':
+            file = glob.glob('./pix/*')
+            file2 = random.choice(file)
+            file3 = discord.File(file2, filename=file2)
+            await message.channel.send(file=file3)   
 
-                # Replace spoiler
-                pattern = r'\|{2}.+?\|{2}'
-                text = re.sub(pattern, '伏せ字', text)
+    if message.content == "$Valo Custom":
+        maps = ["ACENT", "SPLIT", "BIND", "ICEBOX", "BREEZE", "FRACTURE", "HAVEN"]
+        out = random.choice(maps)
+        await message.channel.send(out)        
 
-                # Replace laughing expression
-                if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
-                    while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
-                        text = text[:-1]
-                    text = text[:-1] + '、ワラ'
+    if message.content == '$clip':
+            file = glob.glob('./clip/*')
+            file2 = random.choice(file)
+            file3 = discord.File(file2, filename=file2)
+            await message.channel.send(file=file3)  
 
-                # Add attachment presence
-                for attachment in message.attachments:
-                    if attachment.filename.endswith((".jpg", ".jpeg", ".gif", ".png", ".bmp")):
-                        text += '、画像'
-                    else:
-                        text += '、添付ファイル'
-
-                mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale=1'
-                while message.guild.voice_client.is_playing():
-                    await asyncio.sleep(0.5)
-                source = await discord.FFmpegOpusAudio.from_probe(mp3url)
-                message.guild.voice_client.play(source)
-    await client.process_commands(message)
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    if before.channel is None:
-        if member.id == client.user.id:
-            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-            await client.change_presence(activity=discord.Game(name=presence))
-        else:
-            if member.guild.voice_client is None:
-                await asyncio.sleep(0.5)
-                await after.channel.connect()
-            else:
-                if member.guild.voice_client.channel is after.channel:
-                    text = member.name + 'さんが入室しました'
-                    mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale=1'
-                    while member.guild.voice_client.is_playing():
-                        await asyncio.sleep(0.5)
-                    source = await discord.FFmpegOpusAudio.from_probe(mp3url)
-                    member.guild.voice_client.play(source)
-    elif after.channel is None:
-        if member.id == client.user.id:
-            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-            await client.change_presence(activity=discord.Game(name=presence))
-        else:
-            if member.guild.voice_client:
-                if member.guild.voice_client.channel is before.channel:
-                    if len(member.guild.voice_client.channel.members) == 1:
-                        await asyncio.sleep(0.5)
-                        await member.guild.voice_client.disconnect()
-                    else:
-                        text = member.name + 'さんが退室しました'
-                        mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale=1'
-                        while member.guild.voice_client.is_playing():
-                            await asyncio.sleep(0.5)
-                        source = await discord.FFmpegOpusAudio.from_probe(mp3url)
-                        member.guild.voice_client.play(source)
-    elif before.channel != after.channel:
-        if member.guild.voice_client:
-            if member.guild.voice_client.channel is before.channel:
-                if len(member.guild.voice_client.channel.members) == 1 or member.voice.self_mute:
-                    await asyncio.sleep(0.5)
-                    await member.guild.voice_client.disconnect()
-                    await asyncio.sleep(0.5)
-                    await after.channel.connect()
-
-@client.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, 'original', error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@client.command()
-async def ヘルプ(ctx):
-    message = f'''◆◇◆{client.user.name}の使い方◆◇◆
-{prefix}＋コマンドで命令できます。
-{prefix}接続：ボイスチャンネルに接続します。
-{prefix}切断：ボイスチャンネルから切断します。'''
-    await ctx.send(message)
-
-client.run(token)
+    if message.content == '$差別':
+      embed = discord.Embed(title="NIGGA!",description="黒人死ね！！！！！")
+      await message.channel.send(embed=embed)      
+                            
+client.run('OTY4MTIyODA0MjY2NDkxOTA0.YmaQnw.covHjTTIONxLFEWhWCD-Tp4MQrw')
